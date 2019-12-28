48a9f043266efe291f7f58ab87c5525baab8a902
cuongdhgch17303 <58015761+cuongdhgch17303@users.noreply.github.com>
2019-12-28 14:48:08 +0800

    commit to master

--- connect.php
+++ connect.php
@@ -1,3 +1,3 @@
 <?php
-$conn = pg_connect("host=ec2-174-129-22-22.compute-1.amazonaws.com port=5432 dbname=d37vvq38jkq80h user=elzexzyerwtdyb password=c1fd8e2de951c2c5e2f0c3ae214f41eed89c6a9d2b7e71569203b2d3699f46cc") or die("Connect Unsuccessful");
+$conn = pg_connect("host=ec2-107-21-214-222.compute-1.amazonaws.com port=5432 dbname=d7djob9ch88nfc user=mqlcxpkcveqvkv password=2670b9d9a42d4c10522bbaeb027eee868096bd5a3100311ceab9104a7b384161") or die("Connect Unsuccessful");
 ?>
